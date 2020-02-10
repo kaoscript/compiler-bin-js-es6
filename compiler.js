52d79ad84dc99799c1b6d735bad227e24c95a993
Baptiste Augrain <daiyam@zokugun.org>
2020-02-10 14:43:58 +0100

    fix return checking

--- compiler.js
+++ compiler.js
@@ -5261,6 +5261,7 @@ module.exports = function() {
 				}
 				else {
 					this.next(index - this._index);
+					return null;
 				}
 			}
 			scanIdentifier() {
@@ -5963,6 +5964,7 @@ module.exports = function() {
 						this.throw("]");
 					}
 				}
+				this.throw("]");
 			}
 			altArrayList() {
 				if(arguments.length === 3) {
@@ -15775,17 +15777,20 @@ module.exports = function() {
 			}
 			throw new SyntaxError("Wrong number of arguments");
 		}
-		static __ks_sttc_throwExpectedReturnedValue_0(node) {
-			if(arguments.length < 1) {
-				throw new SyntaxError("Wrong number of arguments (" + arguments.length + " for 1)");
+		static __ks_sttc_throwExpectedReturnedValue_0(type, node) {
+			if(arguments.length < 2) {
+				throw new SyntaxError("Wrong number of arguments (" + arguments.length + " for 2)");
+			}
+			if(type === void 0 || type === null) {
+				throw new TypeError("'type' is not nullable");
 			}
 			if(node === void 0 || node === null) {
 				throw new TypeError("'node' is not nullable");
 			}
-			throw new TypeException("A value is expected to be returned", node);
+			throw new TypeException(KSHelper.concatString("A value of type ", type.toQuote(true), " is expected to be returned"), node);
 		}
 		static throwExpectedReturnedValue() {
-			if(arguments.length === 1) {
+			if(arguments.length === 2) {
 				return TypeException.__ks_sttc_throwExpectedReturnedValue_0.apply(this, arguments);
 			}
 			else if(Exception.throwExpectedReturnedValue) {
@@ -25458,9 +25463,7 @@ module.exports = function() {
 					}
 				}
 			}
-			else {
-				return false;
-			}
+			return false;
 		}
 		isAssignableToVariable() {
 			if(arguments.length === 4) {
@@ -38339,9 +38342,7 @@ module.exports = function() {
 					}
 				}
 			}
-			else {
-				return false;
-			}
+			return false;
 		}
 		isAssignableToVariable() {
 			if(arguments.length === 4) {
@@ -68014,7 +68015,7 @@ module.exports = function() {
 			}
 			else if(this._value === null) {
 				if(!(type.isVoid() === true)) {
-					TypeException.throwExpectedReturnedValue(this);
+					TypeException.throwExpectedReturnedValue(type, this);
 				}
 			}
 			else {
@@ -68829,6 +68830,24 @@ module.exports = function() {
 				return SwitchStatement.prototype.__ks_func_initializeVariable_0.apply(this, arguments);
 			}
 			return Statement.prototype.initializeVariable.apply(this, arguments);
+		}
+		__ks_func_isExit_0() {
+			if(!this._hasDefaultClause) {
+				return false;
+			}
+			for(let __ks_0 = 0, __ks_1 = this._clauses.length, clause; __ks_0 < __ks_1; ++__ks_0) {
+				clause = this._clauses[__ks_0];
+				if(!(clause.body.isExit() === true)) {
+					return false;
+				}
+			}
+			return true;
+		}
+		isExit() {
+			if(arguments.length === 0) {
+				return SwitchStatement.prototype.__ks_func_isExit_0.apply(this);
+			}
+			return Statement.prototype.isExit.apply(this, arguments);
 		}
 		__ks_func_isJumpable_0() {
 			return true;
@@ -96331,11 +96350,8 @@ module.exports = function() {
 				}
 				else if((this._type.isAny() === true) && !(this._type.isExplicit() === true)) {
 				}
-				else if(this._statements.length === 0) {
-					TypeException.throwExpectedReturnedValue(this);
-				}
-				else {
-					this._statements[this._statements.length - 1].checkReturnType(this._type);
+				else if((this._statements.length === 0) || !(__ks_Array._im_last(this._statements).isExit() === true)) {
+					TypeException.throwExpectedReturnedValue(this._type, this);
 				}
 			}
 		}
