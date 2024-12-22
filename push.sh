0982e55bd4cf7a84534eb54cbbf7a2315008c62c
Baptiste Augrain <daiyam@zokugun.org>
2024-12-22 19:57:37 +0100

    vcs: use bash script to push changes

---
+++ push.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+supports_wait() {
+	"$1" --help 2>&1 | grep -q -- '--wait'
+}
+
+if git diff-index --quiet HEAD --; then
+	echo "No changes to commit."
+	exit 0
+fi
+
+git add .
+
+temp_file=$(mktemp /tmp/commit_message.XXXXXX)
+if supports_wait "$EDITOR"; then
+	"$EDITOR" --wait "$temp_file"
+else
+	${EDITOR:-nano} "$temp_file"
+fi
+commit_message=$(cat "$temp_file")
+rm "$temp_file"
+
+git commit -m "$commit_message"
+git push origin
+
+echo "Changes have been committed successfully."
+
+git tag v0.11.0 $( git rev-parse HEAD ) --force
+git push origin --tags --force
